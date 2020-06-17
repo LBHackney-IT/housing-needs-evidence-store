a7dd9ca3d9d0cd88efbe5f8194ebdf9dfec1d6f6
Tom Davies <tom.davies@madetech.com>
2020-06-17 15:20:01 +0100

    Updates docs to include integration testing

--- integration/client/index.ts
+++ integration/client/index.ts
@@ -2,10 +2,12 @@ import EvidenceStoreClient from './EvidenceStoreClient';
 const baseUrl = process.env.INTEGRATION_TEST_BASE_URL;
 
 if (typeof baseUrl !== 'string') {
-  console.error([
-    '[!] Missing integration test configuration!',
-    'You must set "INTEGRATION_TEST_BASE_URL" in your environment.'
+  console.info([
+    '[!] Using default integration test configuration.',
+    'Defaulting to http://localhost:5050, set "INTEGRATION_TEST_BASE_URL".'
   ].join('\n'));
 }
 
-export default new EvidenceStoreClient({ baseUrl });
+export default new EvidenceStoreClient({
+  baseUrl: baseUrl ?? 'http://localhost:5050',
+});
