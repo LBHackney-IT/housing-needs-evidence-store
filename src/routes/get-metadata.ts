ff095c096f5d4260ea198e0bf6ed52cbb2d6d477
Bogdan <bogdan@madetech.com>
2020-07-10 11:40:16 +0100

    Add fastify types

--- src/routes/get-metadata.ts
+++ src/routes/get-metadata.ts
@@ -1,11 +1,16 @@
 import { getMetadata } from '../dependencies';
-import { RouteOptions } from 'fastify';
+import { FastifyReply, RouteOptions, DefaultQuery, FastifyRequest } from 'fastify';
+import { IncomingMessage } from 'http';
+
+interface Params {
+  documentId: string;
+}
 
 const createEndpoint = ({ getMetadata }): RouteOptions => ({
   method: 'GET',
   url: '/:documentId',
-  handler: async (_req, reply) => {
-    const result = await getMetadata.execute(_req.params.documentId);
+  handler: async (req: FastifyRequest<IncomingMessage, DefaultQuery, Params>, reply: FastifyReply) => {
+    const result = await getMetadata.execute(req.params.documentId);
     reply.status(200).send(result);
   },
 });
