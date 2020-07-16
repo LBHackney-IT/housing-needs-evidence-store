23871facd8daf0f5236505869c835d553b7961a0
Kat <katrina@madetech.com>
2020-07-16 09:29:33 +0100

    Add create index to elasticSearch gateway to create an index if it does not yet exist

--- src/gateways/ElasticsearchGateway.test.ts
+++ src/gateways/ElasticsearchGateway.test.ts
@@ -1,6 +1,6 @@
 import elasticsearch from '@elastic/elasticsearch';
 import { NoOpLogger } from '../logging/NoOpLogger';
-import { ElasticsearchGateway } from "./ElasticsearchGateway";
+import { ElasticsearchGateway } from './ElasticsearchGateway';
 
 describe('ElasticsearchGateway', () => {
   const indexName = 'documents';
@@ -10,9 +10,16 @@ describe('ElasticsearchGateway', () => {
   let gateway: ElasticsearchGateway;
 
   beforeEach(() => {
-    client = {
-      index: jest.fn(() => Promise.resolve())
-    } as unknown as elasticsearch.Client;
+    client = ({
+      index: jest.fn(() => Promise.resolve()),
+      cat: {
+        indices: jest.fn((index, callback) => {
+          const statusCode = index.index === 'new_index' ? 404 : 200;
+          callback(null, { statusCode });
+        }),
+      },
+      indices: { create: jest.fn(() => {}) },
+    } as unknown) as elasticsearch.Client;
 
     gateway = new ElasticsearchGateway({
       client,
@@ -28,7 +35,7 @@ describe('ElasticsearchGateway', () => {
       expect(client.index).toHaveBeenCalledWith({
         index: indexName,
         id: metadata.documentId,
-        body: metadata
+        body: metadata,
       });
     });
 
@@ -42,6 +49,41 @@ describe('ElasticsearchGateway', () => {
 
         await expect(gateway.index(metadata)).rejects.toThrow(error);
       });
-    })
+    });
+  });
+
+  describe('#createIndex', () => {
+    it('creates an index if it does not exist', async () => {
+      gateway = new ElasticsearchGateway({
+        client,
+        indexName: 'new_index',
+        logger: new NoOpLogger(),
+      });
+
+      expect(client.cat.indices).toHaveBeenCalledWith(
+        { index: 'new_index' },
+        expect.anything()
+      );
+
+      expect(client.indices.create).toHaveBeenCalledWith(
+        { index: 'new_index' },
+        expect.anything()
+      );
+    });
+
+    it('does not create an index if it already exist', async () => {
+      gateway = new ElasticsearchGateway({
+        client,
+        indexName: 'existing_index',
+        logger: new NoOpLogger(),
+      });
+
+      expect(client.cat.indices).toHaveBeenCalledWith(
+        { index: 'existing_index' },
+        expect.anything()
+      );
+
+      expect(client.indices.create).not.toHaveBeenCalled();
+    });
   });
 });
