5b8e2067e0a46cd8dc89d5376cb66d6261e0762d
Tom Davies <tom.davies@madetech.com>
2020-08-03 11:33:47 +0100

    Adds document contents route to Fastify

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,3 +2,4 @@ export { default as health } from './health';
 export { default as saveMetadata } from './save-metadata';
 export { default as getMetadata } from './get-metadata';
 export { default as findDocuments } from './find-documents';
+export { default as getDocumentContents } from './get-document-contents';
